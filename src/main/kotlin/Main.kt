c4c4d620b499c006b07475650b444d23067eb350
Nikita Chubukov <NikitaCH@bk.ru>
2022-09-03 21:19:02 +0300

    Added an exception for numbers with 11 at the end

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
 fun main() {
-    val likes = 32
-    val result = if (likes % 10 == 1) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
+    val likes = 201
+    val result = if (likes % 10 == 1 && likes % 100 != 11) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
 }
